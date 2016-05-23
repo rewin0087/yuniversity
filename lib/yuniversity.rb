a87fceab8298aa82c593f884ca45768d728fc857
Erwin Seribo <erwin_rulezzz87@yahoo.com>
2016-05-23 19:07:21 +0800

    use json data and stick to university data only

--- lib/yuniversity.rb
+++ lib/yuniversity.rb
@@ -1,27 +1,10 @@
 require "yuniversity/version"
-require 'yaml'
+require 'json'
 
 module YUniversity
-  RAW = YAML.load_file(File.join(File.dirname(__FILE__), 'yuniversity', 'data', 'universities.yml'))
+  RAW = JSON.parse(File.read(File.join(File.dirname(__FILE__), 'yuniversity', 'data', 'universities.json')))
 
-  ALL = RAW.map{|c| c['schools']}.flatten
+  ALL = RAW.map{|u| u['name']}.flatten
 
-  COUNTRIES = RAW.map{|c| {name: c['name'], code: c['iso_code']}}.flatten
-
-  COUNTRY_NAMES = RAW.map{|c| c['name']}.flatten
-
-  NAMES = ALL.map{|u| u['name']}
-
-  def self.find_by_country(country)
-    ALL.select{|c| c['country'].downcase.match(country.downcase)}
-  end
-
-  def self.find_by_countries(countries)
-    ALL.select{|c| countries.map{|e| e.downcase }.include?(c['country'].downcase)}
-  end
-
-  def self.find_country_by_university(university)
-    university = ALL.find{|c| c['name'].downcase.match(university.downcase)}
-    university['country'] unless university.nil?
-  end
+  COUNTRIES = ::ISO3166::Country.all.map{|d| d.data['name']}
 end
--- spec/yuniversity_spec.rb
+++ spec/yuniversity_spec.rb
@@ -4,8 +4,6 @@ describe YUniversity do
   context 'constants' do
     it { expect(YUniversity).to have_constant(:RAW) }
     it { expect(YUniversity).to have_constant(:ALL) }
-    it { expect(YUniversity).to have_constant(:COUNTRIES) }
-    it { expect(YUniversity).to have_constant(:NAMES) }
   end
 
   it '::RAW' do
@@ -20,82 +18,5 @@ describe YUniversity do
     expect(YUniversity::ALL).to be_a Array
     expect(YUniversity::ALL).not_to be_empty
     expect(YUniversity::ALL.size).to eql(16773)
-  end
-
-  it '::COUNTRIES' do
-    expect(YUniversity::COUNTRIES).not_to be_nil
-    expect(YUniversity::COUNTRIES).to be_a Array
-    expect(YUniversity::COUNTRIES).not_to be_empty
-    expect(YUniversity::COUNTRIES.size).to eql(196)
-  end
-
-  it '::COUNTRY_NAMES' do
-    expect(YUniversity::COUNTRY_NAMES).not_to be_nil
-    expect(YUniversity::COUNTRY_NAMES).to be_a Array
-    expect(YUniversity::COUNTRY_NAMES).not_to be_empty
-    expect(YUniversity::COUNTRY_NAMES).to include('Botswana')
-    expect(YUniversity::COUNTRY_NAMES).to include('Burkina Faso')
-    expect(YUniversity::COUNTRY_NAMES).to include('Japan')
-    expect(YUniversity::COUNTRY_NAMES.size).to eql(196)
-  end
-
-  it '::NAMES' do
-    expect(YUniversity::NAMES).not_to be_nil
-    expect(YUniversity::NAMES).to be_a Array
-    expect(YUniversity::NAMES).not_to be_empty
-    expect(YUniversity::NAMES.size).to eql(16773)
-  end
-
-  context '#find_by_country' do
-    it 'PH' do
-      expect(YUniversity.find_by_country('PH')).not_to be_nil
-      expect(YUniversity.find_by_country('PH')).not_to be_empty
-      expect(YUniversity.find_by_country('PH').size).to eql(274)
-    end
-
-    it 'JaPan' do
-      expect(YUniversity.find_by_country('JaPan')).not_to be_nil
-      expect(YUniversity.find_by_country('JaPan')).not_to be_empty
-      expect(YUniversity.find_by_country('JaPan').size).to eql(722)
-    end
-
-    it 'china' do
-      expect(YUniversity.find_by_country('china')).not_to be_nil
-      expect(YUniversity.find_by_country('china')).not_to be_empty
-      expect(YUniversity.find_by_country('china').size).to eql(1300)
-    end
-
-    it 'Test' do
-      expect(YUniversity.find_by_country('Test')).not_to be_nil
-      expect(YUniversity.find_by_country('Test')).to be_empty
-      expect(YUniversity.find_by_country('Test').size).to eql(0)
-    end
-  end
-
-  context '#find_by_countries' do
-    let(:countries) { ['japan', 'korea', 'philippines'] }
-
-    it '[japan, korea, philippines]' do
-      expect(YUniversity.find_by_countries(countries)).not_to be_nil
-      expect(YUniversity.find_by_countries(countries)).not_to be_empty
-      expect(YUniversity.find_by_countries(countries).size).to eql(996)
-    end
-
-    it '[] empty' do
-      expect(YUniversity.find_by_countries([])).not_to be_nil
-      expect(YUniversity.find_by_countries([])).to be_empty
-    end
-  end
-
-  context '#find_country_by_university' do
-    it 'when university is existing - University Ufo Dental' do
-      expect(YUniversity.find_country_by_university('University Ufo Dental')).not_to be_nil
-      expect(YUniversity.find_country_by_university('University Ufo Dental')).not_to be_empty
-      expect(YUniversity.find_country_by_university('University Ufo Dental')).to eql('Albania')
-    end
-
-    it 'when university is not existing University test' do
-      expect(YUniversity.find_country_by_university('University test')).to be_nil
-    end
   end
 end
